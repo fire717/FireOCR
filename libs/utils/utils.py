cdf057e9b1807a89395541dc5536301780d14b07
Mr.Fire <fire15@126.com>
2022-12-19 10:52:48 +0800

    Fix: set benchmark = False

--- libs/utils/utils.py
+++ libs/utils/utils.py
@@ -74,7 +74,7 @@ def setRandomSeed(seed=42):
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = True
+        torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.enabled = True
 
 
